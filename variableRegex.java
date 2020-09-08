e20de21dc30fda70429e79e50a3dfe9926c07cf1
rakeshgupta9764 <rakeshgupta9764@gmail.com>
2020-09-08 19:30:17 +0530

    Create variableRegex.java

---
+++ variableRegex.java
@@ -0,0 +1,36 @@
+/*
+Suppose, you want to create a new programming language that supports variables.
+
+There is a set of rules for identifiers (names) of variables:
+
+It can include lower and upper letters, digits and the underscore character _;
+It can only start with a letter or the underscore;
+If an identifier starts with the underscore, the second character should be either a letter or a digit, but not the underscore;
+Note that a single _ is not a valid identifier.
+
+Using the provided template, write a program that reads n identifiers and then outputs all invalid ones in the same order as they appear in the input data.
+Solution without using regex may be possible.
+*/
+ 
+// Approach using regex
+ 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfVariables = Integer.parseInt(scanner.nextLine());
+
+        String regex = "(_[a-zA-Z0-9]+\\w*)|([a-zA-Z]\\w*)";
+
+        for (int i = 0; i < numberOfVariables; i++) {
+            String identifier = scanner.nextLine();
+            if (!identifier.matches(regex)) {
+                System.out.println(identifier);
+            }
+        }
+    }
+}
+
